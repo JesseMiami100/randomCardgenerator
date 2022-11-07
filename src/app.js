16272a0bab15211f8162cec592b814ba5f12dc33
JesseMiami100 <jessemetayer@gmail.com>
2022-11-07 00:51:17 +0000

    randomCardgenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let card = document.querySelector(".random-card");
+  let rank = document.querySelector(".random-rank");
+
+  let cardSuit = ["hearts", "diamonds", "spades", "clubs"];
+  card.classList.add(cardSuit[getRandomNumber(cardSuit.length)]);
+  let cardRank = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  rank.innerHTML = cardRank[getRandomNumber(cardRank.length)];
+
+  function getRandomNumber(n) {
+    return Math.floor(Math.random() * n);
+  }
 };
